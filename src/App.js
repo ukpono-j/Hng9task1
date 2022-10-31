9a60046e757626e9f03e521b16ccd3b24d33686e
Itoro-Brown <brownitoro814@gmail.com>
2022-10-31 15:55:22 +0100

    HNG Task

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
       <p id='twitter' class='username'>@brown_itoro</p>
       <p id='slackid'>aitee</p>
       <div>
-        <a><button className='decobutton'>Twitter</button></a>
+        <a href='https:twitter.com/brown_itoro'><button className='decobutton'>Twitter</button></a>
         <a href='https://training.zuri.team/'><button id='btn__zuri' class='decobutton'>Zuri Team</button></a>
-        <a href='http://books.zuri.team'><button id='books' class='decobutton'>Zuri Books</button></a>
+        <a href='https://books.zuri.team'><button id='books' class='decobutton'>Zuri Books</button></a>
         <a href='https://books.zuri.team/python-for-beginners?ref_id=<aitee>'><button id='book__python' class='decobutton'>Python Books</button></a>
         <a href='https://background.zuri.team'><button id='pitch' class='decobutton'>Background check for coders</button></a>
         <a href='https://books.zuri.team/design-rules'><button id='book__design' class='decobutton'>Design Books</button></a>
